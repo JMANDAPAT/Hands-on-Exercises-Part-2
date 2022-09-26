98877fb4024a16013ebbfc4085e198d5f4277e1e
JMANDAPAT <114208417+JMANDAPAT@users.noreply.github.com>
2022-09-26 17:37:15 +0800

    Add files via upload

---
+++ PHP exercise 5.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8>"
+<title>Document</title>
+</head>
+<body>
+
+<?php
+$around - "around";
+echo 'What goes ' . $around . ' comes ' . $around . '.';
+?>
+</body>
+</html>
